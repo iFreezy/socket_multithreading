82a3e00c72f434f8f1d08aa5cbfabae5ddca05f5
dkostichev <dkostichev@alfabank.ru>
2018-02-26 15:27:06 +0300

    добавлена логика с остановкой сервера

--- src/ru/httpSocket/Controller.java
+++ src/ru/httpSocket/Controller.java
@@ -2,36 +2,30 @@ package ru.httpSocket;
 
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.util.Scanner;
 
 public class Controller extends Thread {
 
-    ServerSocket serverSocket;
+    public final static int SOCKET_PORT = 13267;
 
-    Controller(ServerSocket ssocket) {
-        // запускаем новый поток
-        this.serverSocket = ssocket;
-        setDaemon(true);
-        setPriority(NORM_PRIORITY);
-        setName("Controller");
-        start();
-    }
+//    public Controller() throws IOException{
+//        try (ServerSocket servsock = new ServerSocket(SOCKET_PORT)) {
+//
+//            while (true) {
+//                System.out.println("Waiting...");
+//                new Server(servsock.accept());
+//            }
+//        }
+//    }
 
     public void run() {
-        while (true) {
-            Scanner in = new Scanner(System.in);
-            System.out.println("echo: " + in.next());
-            if (in.next() == "stop") {
-                try {
-                    Thread.currentThread().interrupt();
-                    serverSocket.close();
-                    System.out.println("Thread name: " + Thread.currentThread().getName());
-                    System.exit(0);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        try (ServerSocket servsock = new ServerSocket(SOCKET_PORT)) {
 
+            while (true) {
+                System.out.println("Waiting...");
+                new Server(servsock.accept());
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
